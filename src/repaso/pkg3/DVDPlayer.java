a49d9c81bdaf35ef6acd698c4e61270054a23fba
Julian- <Julian-@Julian>
2017-02-22 04:53:52 -0500

    Repaso 2

---
+++ src/repaso/pkg3/DVDPlayer.java
@@ -0,0 +1,15 @@
+package repaso.pkg3;
+
+
+public class DVDPlayer {
+    
+    boolean canRecord = false;
+    
+    void recordDVD(){
+        System.out.println("DVD recording");    
+    }   
+    void playDVD(){
+        System.out.println("DVD is playing");
+    }
+     
+}
